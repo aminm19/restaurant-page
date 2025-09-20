e8e0bbdd8f193a9a2e6828318f5bbb5ad6d7f1ac
Amin Mojarad <aminmojarad1@gmail.com>
2025-09-19 21:45:47 -0400

    Replace webpack.config.js
    Add webpack.common.js, webpack.dev.js, and webpack.prod.js
    
    Use webpack-merge to combine common configuration with environment-specific settings
    
    Modify package.json to include new scripts for development and production builds

--- webpack.common.js
+++ webpack.common.js
@@ -1,16 +1,13 @@
-// webpack.config.js
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "development",
   entry: "./src/index.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
-    devtool: "eval-source-map",
     devServer: {
     watchFiles: ["./src/template.html"],
   },
